54fcc12b435f809b47b191bfe51365b3fcfb5457
Javibz <ramonjavi8@gmail.com>
2018-11-01 10:42:47 +0100

    petits changements

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,5 @@ class StaticPagesController < ApplicationController
 
   def home
   end
-  def new
-    @gossips = Gossip.new
-  end
 
-  def create
-  end
-
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  resources :static_pages
   resources :gossips
   get '/:id', to: 'gossips#show'
 end
